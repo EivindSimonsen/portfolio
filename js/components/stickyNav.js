5a89c1106aa7288a74cf7c664cde23af3442c585
EivindSimonsen <eivind.simonsen1998@gmail.com>
2022-03-23 14:33:30 +0100

    Nav is now functionall, sends user to correct segment of page

--- js/components/stickyNav.js
+++ js/components/stickyNav.js
@@ -1,4 +1,8 @@
 const nav = document.querySelector(".navbar");
+const activeHome = document.querySelector(".active-home");
+const activeAbout = document.querySelector(".active-about");
+const activeProjects = document.querySelector(".active-projects");
+const activeContact = document.querySelector(".active-contact");
 
 const sticky = nav.offsetTop;
 
@@ -8,4 +12,26 @@ export function stickyNav() {
     } else {
         nav.classList.remove("sticky");
     }
+};
+
+activeHome.onclick = function() {
+    activeAbout.classList.add("active");
+}
+
+activeAbout.onclick = function() {
+    activeAbout.classList.add("active");
+    activeProjects.classList.remove("active");
+    activeContact.classList.remove("active");
+}
+
+activeProjects.onclick = function() {
+    activeProjects.classList.add("active");
+    activeAbout.classList.remove("active");
+    activeContact.classList.remove("active");
+}
+
+activeContact.onclick = function() {
+    activeContact.classList.add("active");
+    activeProjects.classList.remove("active");
+    activeAbout.classList.remove("active");
 }
--- js/script.js
+++ js/script.js
@@ -1,5 +1,7 @@
 import { stickyNav } from "./components/stickyNav.js";
 
 window.onscroll = function() {
-    stickyNav()
+    stickyNav();
 };
+
+
