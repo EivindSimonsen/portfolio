dfda02cf57f137ac03c9a048e8a1683cac865a8d
EivindSimonsen <eivind.simonsen1998@gmail.com>
2022-03-23 14:59:20 +0100

    With JS i've made the nav add class when active

--- js/components/stickyNav.js
+++ js/components/stickyNav.js
@@ -1,8 +1,10 @@
 const nav = document.querySelector(".navbar");
+const homeBtn = document.querySelector(".home-btn");
 const activeHome = document.querySelector(".active-home");
 const activeAbout = document.querySelector(".active-about");
 const activeProjects = document.querySelector(".active-projects");
 const activeContact = document.querySelector(".active-contact");
+const h2 = document.querySelector(".extra-padding-about-h2 h2");
 
 const sticky = nav.offsetTop;
 
@@ -14,24 +16,34 @@ export function stickyNav() {
     }
 };
 
+homeBtn.onclick = function() {
+    activeAbout.classList.remove("active");
+    activeProjects.classList.remove("active");
+    activeContact.classList.remove("active");
+}
+
 activeHome.onclick = function() {
     activeAbout.classList.add("active");
+    h2.style.padding = "100px 0px 0px 0px";
 }
 
 activeAbout.onclick = function() {
     activeAbout.classList.add("active");
     activeProjects.classList.remove("active");
     activeContact.classList.remove("active");
+    h2.style.padding = "0px";
 }
 
 activeProjects.onclick = function() {
     activeProjects.classList.add("active");
     activeAbout.classList.remove("active");
     activeContact.classList.remove("active");
+    h2.style.padding = "100px 0px 0px 0px";
 }
 
 activeContact.onclick = function() {
     activeContact.classList.add("active");
     activeProjects.classList.remove("active");
     activeAbout.classList.remove("active");
+    h2.style.padding = "100px 0px 0px 0px";
 }
