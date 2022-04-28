46a8c401d2b53e02fe818939a4c605bd92d70ee5
EivindSimonsen <eivind.simonsen1998@gmail.com>
2022-04-29 01:04:46 +0200

    Form will now send me mails

--- js/components/formValidation.js
+++ js/components/formValidation.js
@@ -5,12 +5,11 @@ export function validateForm() {
     const email = document.querySelector("#email");
     const subject = document.querySelector("#subject");
     const message = document.querySelector("#message");
-    const checkbox = document.querySelector(".g-recaptcha");
 
     function validateFormInputs(event) {
         event.preventDefault();
 
-        if (fullName.value.trim().length > 0 && validateEmail(email.value) === true && subject.value.trim().length > 0 && message.value.trim().length > 0 && checkbox.checked === true) {
+        if (fullName.value.trim().length > 0 && validateEmail(email.value) === true && subject.value.trim().length > 0 && message.value.trim().length > 0) {
             document.querySelector(".disabled").disabled = false;
         } else {
             document.querySelector(".disabled").disabled = true;
