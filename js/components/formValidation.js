9ec2b56de91bc903a8ea5a8683a844c1b66bd71e
EivindSimonsen <eivind.simonsen1998@gmail.com>
2022-04-29 00:53:22 +0200

    test2

--- js/components/formValidation.js
+++ js/components/formValidation.js
@@ -5,12 +5,12 @@ export function validateForm() {
     const email = document.querySelector("#email");
     const subject = document.querySelector("#subject");
     const message = document.querySelector("#message");
-    const checkbox = document.querySelector(".checkbox");
+    const checkbox = document.querySelector(".field");
 
     function validateFormInputs(event) {
         event.preventDefault();
 
-        if (fullName.value.trim().length > 0 && validateEmail(email.value) === true && subject.value.trim().length > 0 && message.value.trim().length > 0) {
+        if (fullName.value.trim().length > 0 && validateEmail(email.value) === true && subject.value.trim().length > 0 && message.value.trim().length > 0 && checkbox.clicked) {
             document.querySelector(".disabled").disabled = false;
         } else {
             document.querySelector(".disabled").disabled = true;
