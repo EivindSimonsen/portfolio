1b61405ad85934d99cce0a16b6700d4b9d013841
EivindSimonsen <eivind.simonsen1998@gmail.com>
2022-04-29 00:58:21 +0200

    test4

--- js/components/formValidation.js
+++ js/components/formValidation.js
@@ -5,12 +5,12 @@ export function validateForm() {
     const email = document.querySelector("#email");
     const subject = document.querySelector("#subject");
     const message = document.querySelector("#message");
-    const checkbox = document.querySelector(".field");
+    const checkbox = document.querySelector(".recaptcha-checkbox");
 
     function validateFormInputs(event) {
         event.preventDefault();
 
-        if (fullName.value.trim().length > 0 && validateEmail(email.value) === true && subject.value.trim().length > 0 && message.value.trim().length > 0 && checkbox.checked) {
+        if (fullName.value.trim().length > 0 && validateEmail(email.value) === true && subject.value.trim().length > 0 && message.value.trim().length > 0 && checkbox.checked === true) {
             document.querySelector(".disabled").disabled = false;
         } else {
             document.querySelector(".disabled").disabled = true;
