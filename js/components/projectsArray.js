ce815fa2fd6a195ffa6a7edc6553fbb7dab25cd3
EivindSimonsen <eivind.simonsen1998@gmail.com>
2023-01-27 23:54:37 +0100

    Added new project

--- js/components/projectsArray.js
+++ js/components/projectsArray.js
@@ -1,9 +1,17 @@
 export const projects = [
   {
+    name: "React E-commerce website",
+    content: "This project was made in my freetime to develop my skills in React further. It features state management from several components to store and send shopping cart data. It also features a gallery with a modal functionality. Styles was made with SASS.",
+    image: "../../img/react-ecommerce.JPG",
+    modalNr: "modal-five",
+    websiteLink: "https://react-ecommerce-eas.netlify.app/",
+    githubLink: "https://github.com/eivindsimonsen/react-ecommerce",
+  },
+  {
     name: "Personalforeningen i Coop Øst",
     content: "My boss reached out and said he needed a website for Coop Øst's own staff association. Me and a fellow developer set out for the challenge. The websites purpose is to give information about what the association can offer for employees in Coop Øst. The association offers among other things, cheaper cabin rental. The website is built with REACT and SASS as pre-processor, with wordpress REST api for content. My boss can now fully change the content on the page, without reaching out to the devs.",
     image: "../../img/personalforeningen.JPG",
-    modalNr: "modal-six",
+    modalNr: "modal-four",
     websiteLink: "https://personalforeningen.com/",
     githubLink: "https://github.com/emiost/personal",
   },
@@ -11,12 +19,12 @@ export const projects = [
     name: "Holidaze - Accommodation booking site",
     content: "This was my final exam, tasked in creating a booking website for accommodations in Bergen, Norway. This project includes a searchbar typeahead, fetching items from wordpress REST api, login possibility and contacting possibilites. This project is ment to represent my entire learing curve as a student at Noroff through the years.",
     image: "../../img/holidaze.JPG",
-    modalNr: "modal-five",
+    modalNr: "modal-three",
     websiteLink: "https://holidazeeivindsimonsen.netlify.app/",
     githubLink: "https://github.com/Noroff-FEU-Assignments/project-exam-2-eivindsimonsen",
   },
   {
-    name: "Discord website clone",
+    name: "Discord landing-page clone",
     content: "This project was for practicing my react skills with use of components and stylings. My goal was to design the home page of the discord website without getting help from inspecting their code.",
     image: "../../img/discord.JPG",
     modalNr: "modal-two",
